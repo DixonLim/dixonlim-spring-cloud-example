6e428b44c425996b83d03122a425f380e304a70c
DixonLim <DixonLim@qq.com>
2020-06-21 23:59:04 +0800

    1.提交代码保存到git

--- dixonlim-spring-cloud-config/dixonlim-spring-cloud-config-client/src/main/java/com/dixonlim/ConfigClientApp.java
+++ dixonlim-spring-cloud-config/dixonlim-spring-cloud-config-client/src/main/java/com/dixonlim/ConfigClientApp.java
@@ -2,6 +2,9 @@ package com.dixonlim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Auther: DixonLim
@@ -9,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description:
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApp.class, args);
--- dixonlim-spring-cloud-config/dixonlim-spring-cloud-config-server/src/main/java/com/dixonlim/ConfigServerApp.java
+++ dixonlim-spring-cloud-config/dixonlim-spring-cloud-config-server/src/main/java/com/dixonlim/ConfigServerApp.java
@@ -2,6 +2,7 @@ package com.dixonlim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Auther: DixonLim
@@ -10,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient // 通过注解@EnableEurekaClient 表明自己是一个eureka client
 public class ConfigServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApp.class, args);
